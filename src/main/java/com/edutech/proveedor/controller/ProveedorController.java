d68c1409feef5e95e5631461ca9d7380c8441bd3
Ricardo Novoa Ll. <ri.novoa@duocuc.cl>
2025-06-22 19:46:09 -0400

    Ajustes en proveedor-service: correcciones en controller y service, relaciones tambien

--- src/main/java/com/edutech/proveedor/controller/ProveedorController.java
+++ src/main/java/com/edutech/proveedor/controller/ProveedorController.java
@@ -3,14 +3,22 @@ package com.edutech.proveedor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.edutech.proveedor.model.Proveedor;
 import com.edutech.proveedor.service.ProveedorService;
 
 @RestController
-@RequestMapping("/api/v1/proveedores")
+@RequestMapping("/")
 public class ProveedorController {
 
     @Autowired
